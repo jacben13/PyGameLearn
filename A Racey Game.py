ad08866f589ed201d6c2b6d3201b9789987b0a4b
Ben Jacobs <jacben13@gmail.com>
2015-01-28 13:14:40 -0800

    completed lesson 3

--- A Racey Game.py
+++ A Racey Game.py
@@ -31,13 +31,31 @@ def car(x,y):
 x = (display_width * 0.45)
 y = (display_height * 0.8)
 
+x_change = 0
+
 crashed = False
 
 while not crashed:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            if event.key == pygame.K_RIGHT:
+                x_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                if not x_change == 5:
+                    x_change = 0
+            if event.key == pygame.K_RIGHT:
+                if not x_change == -5:
+                    x_change = 0
+
+    x += x_change
     gameDisplay.fill(white)
     car(x,y)
     pygame.display.update()
