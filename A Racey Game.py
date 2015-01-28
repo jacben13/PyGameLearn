71edfde0255dd92ec91c6f0f12831f309fe9c5c0
Ben Jacobs <jacben13@gmail.com>
2015-01-27 21:53:45 -0800

    Initial commit

---
+++ A Racey Game.py
@@ -0,0 +1,30 @@
+__author__ = 'Ben'
+# First lesson on PyGame from Sentdex https://www.youtube.com/watch?v=ujOTNg17LjI
+
+import pygame
+
+pygame.init()
+
+# Set the size of the display
+gameDisplay = pygame.display.set_mode((800,600))
+
+# Set Window Title
+pygame.display.set_caption('A bit Racey')
+
+# Define game clock for our game
+clock = pygame.time.Clock()
+
+crashed = False
+
+while not crashed:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit()
---
+++ README.md
@@ -0,0 +1,13 @@
+PyGameLearn
+==============
+
+Project I am using to learn about PyGame.
+I am following a YouTube tutorial series from Sentdex, the first lesson is [here](https://www.youtube.com/watch?v=ujOTNg17LjI)
+
+I am currently using Python 3.4.1 for this project.
+
+Current external libraries I am using are:
+[PyGame](http://www.lfd.uci.edu/~gohlke/pythonlibs/#pygame)
+
+For PyGame I am using the unofficial library found at:
+http://www.lfd.uci.edu/~gohlke/pythonlibs/#pygame
