d4a68d29403aa7dc5a06d5edd5fe18bb722d24c3
Ben Jacobs <jacben13@gmail.com>
2015-01-27 23:07:53 -0800

    completed lesson 2

--- A Racey Game.py
+++ A Racey Game.py
@@ -5,14 +5,31 @@ import pygame
 
 pygame.init()
 
+# Window resolution
+display_width = 800
+display_height = 600
+
+# Color definitions
+black = (0, 0, 0)
+white = (255, 255, 255)
+
 # Set the size of the display
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((display_width, display_height))
 
 # Set Window Title
 pygame.display.set_caption('A bit Racey')
 
 # Define game clock for our game
 clock = pygame.time.Clock()
+
+carImg = pygame.image.load('racecar.png')
+
+# Helper function to blit the car image
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+x = (display_width * 0.45)
+y = (display_height * 0.8)
 
 crashed = False
 
@@ -21,10 +38,10 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
-
+    gameDisplay.fill(white)
+    car(x,y)
     pygame.display.update()
     clock.tick(60)
-#test
+
 pygame.quit()
 quit()
